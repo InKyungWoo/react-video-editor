5d5a8b2270e2d82db27128ece16b760d5a08dc11
InKyungWoo <chelsea1201@naver.com>
2024-07-01 04:01:42 +0900

    :art: design: logo - light ver. 변경

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import logo from "../assets/video-editor.png";
+import logo from "../assets/logo_light.png";
 
 const Header = () => {
   return (
