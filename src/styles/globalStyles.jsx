1a27fff40c851d0e240d8803458e8d1fc48a7bbe
InKyungWoo <chelsea1201@naver.com>
2024-06-30 23:49:52 +0900

    :art: design: globalstyle, favicon 수정

---
+++ src/styles/globalStyles.jsx
@@ -0,0 +1,30 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    font-family: 'Noto Sans KR', sans-serif;
+    background-color: #f8f9fa;
+  }
+
+  #root {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .container {
+    width: 100%;
+    max-width: 1280px;
+    padding: 0 1rem;
+    margin: 0 auto;
+  }
+
+`;
+
+export default GlobalStyle;
